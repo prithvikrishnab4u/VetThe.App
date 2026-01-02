a6f1081ded195ef46f7c5dda430ada40fe228960
prithvikrihsnab4u <prithvikrishnab4u@gmail.com>
2026-01-02 01:06:41 -0800

    good table and formating keep it jan 2 morning

---
+++ site/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./layouts/**/*.html",
+        "./content/**/*.md",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+}
